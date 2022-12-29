7881eb423cc8e14f26954c5eda2f82ca5a9ff69a
JenniferM-BShulyak <111457464+JenniferM-BShulyak@users.noreply.github.com>
2022-12-29 15:50:15 -0600

    Added data import code to table schema script

--- EmployeeSQL/table_schema.sql
+++ EmployeeSQL/table_schema.sql
@@ -1,6 +1,6 @@
 -- Create table for employees
 CREATE TABLE employees (
-  emp_no INT,
+  emp_no INT NOT NULL,
   emp_title_id VARCHAR(10),
   birth_date VARCHAR(10),
   first_name VARCHAR(30),
@@ -17,7 +17,7 @@ CREATE TABLE titles (
 
 -- Create table for employees by department
 CREATE TABLE dept_emp (
-  emp_no INT,
+  emp_no INT NOT NULL,
   dept_no VARCHAR(10),
 );
 
@@ -38,7 +38,42 @@ CREATE TABLE dept_manager (
 
 -- Create table for salaries
 CREATE TABLE salaries (
-  emp_no INT,
+  emp_no INT NOT NULL,
   salary INT,
 );
 
+-- Import data for employees
+COPY employees
+FROM '/Users/jennifershulyak/Desktop/Data_Bootcamp_2022/Challenges/sql-challenge/Data/employees.csv'
+DELIMITER ','
+CSV HEADER;
+
+-- Import data for employee titles
+COPY titles
+FROM '/Users/jennifershulyak/Desktop/Data_Bootcamp_2022/Challenges/sql-challenge/Data/titles.csv'
+DELIMITER ','
+CSV HEADER;
+
+-- Import data for employees by department
+COPY dept_emp
+FROM '/Users/jennifershulyak/Desktop/Data_Bootcamp_2022/Challenges/sql-challenge/Data/dept_emp.csv'
+DELIMITER ','
+CSV HEADER;
+
+-- Import data for departments
+COPY departments
+FROM '/Users/jennifershulyak/Desktop/Data_Bootcamp_2022/Challenges/sql-challenge/Data/departments.csv'
+DELIMITER ','
+CSV HEADER;
+
+-- Import data for department managers
+COPY employees
+FROM '/Users/jennifershulyak/Desktop/Data_Bootcamp_2022/Challenges/sql-challenge/Data/dept_manager.csv'
+DELIMITER ','
+CSV HEADER;
+
+-- Import data for employees
+COPY salaries
+FROM '/Users/jennifershulyak/Desktop/Data_Bootcamp_2022/Challenges/sql-challenge/Data/salaries.csv'
+DELIMITER ','
+CSV HEADER;
