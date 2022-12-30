caa92cb768c782f5de85392cd6f8221386e851dd
JenniferM-BShulyak <111457464+JenniferM-BShulyak@users.noreply.github.com>
2022-12-30 15:19:00 -0600

    Finished analysis 7,8

--- EmployeeSQL/analysis.sql
+++ EmployeeSQL/analysis.sql
@@ -38,3 +38,25 @@ WHERE emp_no IN (
 		FROM departments
 		WHERE dept_name = 'Sales'))
 ;
+
+
+-- (7) List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees e
+JOIN dept_emp de ON e.emp_no = de.emp_no
+LEFT JOIN departments d ON de.dept_no = d.dept_no
+WHERE e.emp_no IN (
+	SELECT emp_no
+	FROM dept_emp
+	WHERE dept_no IN (
+		SELECT dept_no
+		FROM departments
+		WHERE dept_name = 'Sales' OR dept_name = 'Development'))
+;
+
+-- (8) List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
+SELECT last_name, COUNT(last_name) AS frequency_counts
+FROM employees
+GROUP BY last_name
+ORDER BY frequency_counts DESC
+;
